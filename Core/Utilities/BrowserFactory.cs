6bd5a62dbfc51c357a724a7666ae6c7003c1f37e
fsilva <frank.silva.castro.1@gmail.com>
2025-08-26 11:47:52 -0500

    test

--- Core/Utilities/BrowserFactory.cs
+++ Core/Utilities/BrowserFactory.cs
@@ -19,10 +19,10 @@ namespace Core.Utilities
                     // TODO Options can be created in a provider
                     var guid = Guid.NewGuid();
                     var options = new ChromeOptions();
-                    options.AddUserProfilePreference("download.default_directory", "/tmp/chrome-user-data-{guid}");
+                    //options.AddUserProfilePreference("download.default_directory", "/tmp/chrome-user-data-{guid}");
                     options.AddUserProfilePreference("download.prompt_for_download", false);
                     options.AddUserProfilePreference("disable-popup-blocking", true);
-                    options.AddArgument($"--user-data-dir=/tmp/chrome-user-data-{guid}");
+                    //options.AddArgument($"--user-data-dir=/tmp/chrome-user-data-{guid}");
 
                     if (headless)
                     {
