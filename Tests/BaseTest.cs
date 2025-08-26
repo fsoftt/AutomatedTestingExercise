e0bcb1facfcb91965502a6125aa743762543f4e7
fsilva <frank.silva.castro.1@gmail.com>
2025-08-26 12:53:42 -0500

    test

--- Tests/BaseTest.cs
+++ Tests/BaseTest.cs
@@ -4,6 +4,7 @@ using CrossCutting.Providers;
 using CrossCutting.Static;
 using CrossCutting.Types;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using NUnit.Framework.Interfaces;
 using OpenQA.Selenium;
 
@@ -18,15 +19,29 @@ namespace Tests
         [SetUp]
         public void Setup()
         {
+            var logger = LoggingFactory.CreateLogger<BaseTest>();
+
+            logger.LogDebug("Setting up the test.");
             IConfiguration testData = serviceProvider.GetTestData();
+            logger.LogDebug("Retrieved test data.");
             IConfiguration configuration = serviceProvider.GetConfiguration();
-            
+            logger.LogDebug("Retrieved configuration.");
+
             string url = testData.GetValue<string?>(ConfigurationKeys.Url)!;
-            string browser = configuration.GetValue<string?>(ConfigurationKeys.Browser)!;
+            string browser = Environment.GetEnvironmentVariable("TEST_BROWSER")?.ToLower() ?? configuration.GetValue<string?>(ConfigurationKeys.Browser)!;
+            logger.LogDebug("Browser to use: " + browser);
 
+            logger.LogDebug("Creating driver");
             driver = serviceProvider.GetWebDriver(BrowserTypeFactory.FromString(browser));
+            logger.LogDebug("Driver created");
 
+            logger.LogDebug("Navigating");
             driver.Navigate().GoToUrl(url);
+            logger.LogDebug("Navigated");
+
+            logger.LogDebug("Taking screenshot");
+            ScreenshotProvider.TakeBrowserScreenshot(driver);
+            logger.LogDebug("Screenshot taken");
 
             homePage = new HomePage(driver, serviceProvider);
         }
